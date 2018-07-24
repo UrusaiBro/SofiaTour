a0265900d91c0c0be79ee4df0ce0b6534905a5ba
Vasil Uvardov <Vasil.Uvardov@gmail.com>
2018-07-24 17:00:51 +0300

    Logging in initial implementation : sign in working!

--- app/src/main/java/com/mbtutu/sofiatour/MainActivity.java
+++ app/src/main/java/com/mbtutu/sofiatour/MainActivity.java
@@ -1,15 +1,23 @@
 package com.mbtutu.sofiatour;
 
 import android.os.Bundle;
+import java.util.*;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private static final int RC_SIGN_IN = 123;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -35,6 +43,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //IF not logged in: log in
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+
+            List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
+            // starts sign in
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        } else {  // Else: display toast
+            Toast.makeText(this, "Welcome " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_LONG).show();
+        }
+
+
+// Create and launch sign-in intent
+
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
