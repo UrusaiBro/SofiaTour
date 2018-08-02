ef7a85ea45101fdb9e0f421b1aab15c3411e649c
legobatkata <legobatkata@abv.bg>
2018-08-02 11:54:50 +0300

    Merge branch 'master' of https://github.com/UrusaiBro/SofiaTour
    # Conflicts:
    #	app/src/main/java/com/mbtutu/sofiatour/MainActivity.java

--- app/src/main/java/com/mbtutu/sofiatour/MainActivity.java
+++ app/src/main/java/com/mbtutu/sofiatour/MainActivity.java
@@ -1,26 +1,33 @@
 package com.mbtutu.sofiatour;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import java.io.InputStream;
 import java.util.*;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ActionMenuItemView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
     //private Button bAcc;
     Button tours_btn, map_btn, sights_btn, profile_btn;
+    ImageView toolbar_imgview;
 
     /*
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -56,25 +64,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //bAcc = findViewById(R.id.logout);
-        //IF not logged in: log in
-        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
-            signIn();
-            //bAcc.setText("Sign Out");
-        } else {  // Else: display toast
-            Toast.makeText(this, "Welcome " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_LONG).show();
-        }
-
-        //mTextMessage = findViewById(R.id.message);
-        //BottomNavigationView navigation = findViewById(R.id.navigation);
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
 
 
         tours_btn = findViewById(R.id.tours_btn);
         map_btn = findViewById(R.id.map_btn);
         sights_btn = findViewById(R.id.sights_btn);
         profile_btn = findViewById(R.id.profile_btn);
+
+        toolbar_imgview = findViewById(R.id.toolbar_imgview);
+
+        //IF not logged in: log in
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            signIn();
+        } else {
+            // Else: display toast
+            Toast.makeText(this, "Welcome " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_LONG).show();
+
+            new DownloadImageTask(toolbar_imgview).execute(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString());
+        }
+
+
+
+
+
+
 
 
         tours_btn.setOnClickListener(new View.OnClickListener() {
@@ -133,7 +146,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    // method to download profile pic
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
 
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 
     private void signIn() {
         List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
@@ -141,6 +178,7 @@ public class MainActivity extends AppCompatActivity {
         // starts sign in
         startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN);
     }
+
 
 
 }
